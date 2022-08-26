b770d9846db484cb6e6534c28d3ffd79bb115f71
Ivana Siljanoska <jancevska.ivana@gmail.com>
2022-08-26 15:40:29 +0200

    proba verzija

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import JsonData from './data/data.json'
+import './App.css'
+import { Navigation } from "./components/Navigation";
+import { Footer } from "./components/Footer";
+import { Awards } from "./components/Awards";
+import {Route, Routes} from 'react-router-dom'
+import { HomePage } from "./components/HomePage";
+import { Certificates } from "./components/Certificates";
+
+
+
+// export const scroll = new SmoothScroll('a[href*="#"]', {
+//   speed: 1000,
+//   speedAsDuration: true,
+// });
+
+export function App() {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+  return (
+    <div>
+      <Navigation />
+      
+      <Routes >
+        <Route path="/" element={<HomePage />} />
+        <Route path="/awards" element={<Awards data={landingPageData.Awards} />} />
+        <Route path="/certificates" element={<Certificates data={landingPageData.Certificates} />} />
+      
+      </Routes>
+      
+      <Footer data={landingPageData.Footer} />
+
+    </div>
+  )
+}
---
+++ src/components/Awards.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from "react";
+
+import '../css-components/awards.css'
+import card1 from '../images/award1.jpg'
+import card2 from '../images/award2.jpg'
+import card3 from '../images/award3.jpg'
+import { PopUp } from "./PopUp";
+import '../css-components/certificates.css'
+import JsonData from '../data/data.json'
+
+export const Awards = (props) => {
+  const [landingPageData, setLandingPageData] = useState({});
+  useEffect(() => {
+    setLandingPageData(JsonData);
+  }, []);
+
+
+  return (
+
+    <div id="awards" className='text-center'>
+      {props.data
+        ? props.data.map((d, i) => (
+
+
+          <article class="card" key={`${d.title}-${i}`}  >
+            <header class="card__thumb">
+              <a><img src={card1} /></a>
+            </header>
+            <div class="card__body">
+              <div class="card__category"><a ><b>A w a r d</b></a></div>
+              <h2 class="card__title"><a >{d.title}</a></h2>
+              <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+                more
+              </button>
+              <div className='row'>
+
+                <div className='portfolio-items'>
+
+                  <PopUp data={landingPageData.Awards} />
+                </div>
+              </div>
+              {/* <div class="card__subtitle">more </div> */}
+              <p class="card__description">
+                {d.description}
+              </p>
+            </div>
+            <footer class="card__footer">
+              <span class="icon ion-clock"></span> 10 mins ago
+              <span class="icon ion-chatbox"></span><a href="#"> 145 comments</a>
+            </footer>
+          </article>
+        ))
+        : "loading"}
+
+
+
+
+    </div>
+
+  )
+}
